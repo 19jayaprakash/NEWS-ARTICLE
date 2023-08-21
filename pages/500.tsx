e4eb9f632ac83f7bccb1e1d8468f2de185c4e990
ktmjk <101854990+ktmjk@users.noreply.github.com>
2023-08-21 12:07:01 +0530

    Commit

---
+++ pages/500.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div>
+        <h1>Error</h1>
+        <p>Looks like something went wrong. Please refresh the page or contact the administrator</p>
+    </div>
+  )
+}
+
+export default ErrorPage
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,8 @@
 import NewsArticlesGrid from "@/components/NewsArticlesGrid";
 import { NewsArticle } from "@/models/NewsArticles";
+import Head from "next/head";
 import { FormEvent, useState } from "react"
-import {Form,Button,Spinner} from 'react-bootstrap'
+import {Form,Button,Spinner, Alert} from 'react-bootstrap'
 
 const SearchNewsPage = () => {
 const [searchResults,setSearchResults] =useState<NewsArticle[] | null >(null);
@@ -33,23 +34,32 @@ async function handleSubmit(e:FormEvent<HTMLFormElement>) {
 }
 
   return (
-    <main>
-    <h1>Search News</h1>  
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="search-input">
-        <Form.Label>Search query</Form.Label>
-        <Form.Control name="searchQuery"
-        placeholder="E.g. product,plants"/>
-      </Form.Group>
-      <Button type="submit" className="mb-3" disabled={searchResultsLoading}>Search</Button>
-    </Form>
-    <div className="d-flex flex-column align-items-center">
-      {searchResultsLoading && <Spinner animation="border"/>}
-      {searchResultsLoadingIsError && <p>Something went wrong. Please try again.</p>}
-      {searchResults?.length===0 && <p>Nothing found. Try a different query.</p>}
-      {searchResults && <NewsArticlesGrid articles={searchResults}/>}
-    </div>
-    </main>
+    <>
+    <Head>
+      <title key="title">Search News - NextJS News App</title>
+    </Head>
+      <main>
+      <h1>Search News</h1>  
+      <Alert>
+        This is page uses <strong>client-side data fetchimg</strong>to show fresh data for every search.
+        Requests are handles by our backend via <strong>API routes</strong>.
+      </Alert>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="search-input">
+          <Form.Label>Search query</Form.Label>
+          <Form.Control name="searchQuery"
+          placeholder="E.g. sports, plants, ..."/>
+        </Form.Group>
+        <Button type="submit" className="mb-3" disabled={searchResultsLoading}>Search</Button>
+      </Form>
+      <div className="d-flex flex-column align-items-center">
+        {searchResultsLoading && <Spinner animation="border"/>}
+        {searchResultsLoadingIsError && <p>Something went wrong. Please try again.</p>}
+        {searchResults?.length===0 && <p>Nothing found. Try a different query.</p>}
+        {searchResults && <NewsArticlesGrid articles={searchResults}/>}
+      </div>
+      </main>
+    </>
   )
 }
 
